1263a3e0a8aea91ad9f013f1130e4917c5a50166
LeCroder <83643281+LeCroder@users.noreply.github.com>
2021-05-07 21:12:36 +0200

    game
    nouvel mise

---
+++ Assets/JumpScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+    //Variable
+    Rigidbody rig;
+    public float speed = 10f;
+    bool jumping;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            jumping = true;
+    }
+
+    private void FixedUpdate()
+    {
+        // Rigidbody needs to be coded
+       if (jumping)
+       {
+            rig.AddForce(transform.up * speed, ForceMode.VelocityChange);
+            isGrounded = false;
+            jumping = false;
+       }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
